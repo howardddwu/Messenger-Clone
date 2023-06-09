44bc7454c3cbd4b32c7646e2142f974eb932be9c
haoyuan wu <howardddwu@gmail.com>
2023-06-09 14:18:54 -0400

    auth ui done

---
+++ app/(site)/component/AuthForm.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState(true); // true is LOGIN, false is REGISTER
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    setVariant(!variant);
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === false) {
+      //Axios Register
+    }
+
+    if (variant === true) {
+      //NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div
+      className="
+        mt-8
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md
+  "
+    >
+      <div
+        className="
+        bg-white
+        px-4
+        py-8
+        shadow
+        sm:rounded-lg
+        sm:px-10"
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === false && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === true ? "Sign in" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+              absolute
+              inset-0
+              flex
+              items-center
+            "
+            >
+              <div
+                className="
+                  w-full 
+                  border-t 
+                  border-gray-300"
+              />
+            </div>
+            <div
+              className="
+              relative
+              flex
+              justify-center
+              text-sm
+            "
+            >
+              <span
+                className="
+              bg-white
+              px-2 
+              text-gray-500"
+              >
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div
+            className="
+            mt-6
+            flex
+            gap-2
+          "
+          >
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+          <div
+            className="
+            flex
+            gap-2
+            justify-center
+            text-sm
+            mt-6
+            px-2
+            text-gray-500
+          "
+          >
+            <div>
+              {variant === true
+                ? "New to Messenger?"
+                : "Already have an account?"}
+            </div>
+            <div onClick={toggleVariant} className="underline cursor-pointer">
+              {variant === true ? "Create an account" : "Login"}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
---
+++ app/(site)/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import AuthForm from "./component/AuthForm";
+
+export default function Home() {
+  return (
+    <div
+      className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+    "
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-x-md">
+        <Image
+          alt="Logo"
+          height="48"
+          width="48"
+          className="w-auto mx-auto"
+          src="/image/logo.png"
+        />
+      </div>
+      <h2
+        className="
+        mt-6 
+        text-center
+        text-3xl
+        font-bold
+        tracking-tight
+        text-gray-900
+      "
+      >
+        Sign in to your account
+      </h2>
+      <AuthForm />
+    </div>
+  );
+}
